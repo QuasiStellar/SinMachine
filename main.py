89e48d07c645f7aca57d06b77ee97b7adb4df93c
QuasiStellar <quasistellar.official@gmail.com>
2021-06-05 21:28:10 +0300

    generators and bugfixes

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
+DEBUG = False
 MAP = "⬛⬛⬛⬛⬛" \
-      "⬛⬜▧⬜⬛" \
-      "⬛▧⬜⬜⬛" \
+      "⬛⬜⬜⬜⬛" \
+      "⬛▧⬜◐⬛" \
+      "⬛⬜⬜⬜⬛" \
       "⬛⬛⬛⬛⬛"
 MAP = list(MAP)
 WIDTH = 5
 HEIGHT = len(MAP) // WIDTH
 POS_X = 1
 POS_Y = 1
-TURN_LIMIT = 4
-V_CELL = True
+TURN_LIMIT = 3
+V_CELL = False
 V_CELL_X = 3
 V_CELL_Y = 2
 GEN_COUNT = MAP.count('◐')
@@ -18,18 +20,21 @@ conditions = []
 
 
 class Condition:
-    def __init__(self, scheme, pos_x, pos_y, turn, route, done):
+    def __init__(self, scheme, pos_x, pos_y, turn, route, gen_count, done):
         self.scheme = scheme
         self.pos_x = pos_x
         self.pos_y = pos_y
         self.turn = turn
         self.route = route
+        self.gen_count = gen_count
         self.done = done
         if V_CELL and self.pos_x == V_CELL_X and self.pos_y == V_CELL_Y:
             self.done = True
-        elif self.turn > TURN_LIMIT:
+        if not V_CELL and gen_count == 0:
+            self.done = True
+        elif self.turn >= TURN_LIMIT:
             pass
-        elif abs(self.pos_x - V_CELL_X) + abs(self.pos_y - V_CELL_Y) > TURN_LIMIT - self.turn:
+        elif V_CELL and abs(self.pos_x - V_CELL_X) + abs(self.pos_y - V_CELL_Y) > TURN_LIMIT - self.turn:
             pass
         else:
             self.generate_new()
@@ -43,9 +48,11 @@ class Condition:
         """
         neighbours = []
         for direction in ((-1, 0), (0, -1), (1, 0), (0, 1)):
-            if self.scheme[(self.pos_y + direction[1]) * WIDTH + self.pos_x + direction[0]] == '⬜' and \
-                    not (self.route[-1][1] == 0 and
-                         (direction[0] + self.route[-1][0][0] == 0 and direction[1] + self.route[-1][0][1] == 0)):
+            if self.scheme[(self.pos_y + direction[1]) * WIDTH + self.pos_x + direction[0]] == '◐':
+                return [(direction, 2)]
+            elif self.scheme[(self.pos_y + direction[1]) * WIDTH + self.pos_x + direction[0]] == '⬜' and \
+                    (len(self.route) == 0 or not (self.route[-1][1] == 0 and
+                         (direction[0] + self.route[-1][0][0] == 0 and direction[1] + self.route[-1][0][1] == 0))):
                 neighbours.append((direction, 0))
             elif self.scheme[(self.pos_y + direction[1]) * WIDTH + self.pos_x + direction[0]] == '▧' and \
                     self.scheme[(self.pos_y + 2 * direction[1]) * WIDTH + self.pos_x + 2 * direction[0]] == '⬜':
@@ -54,28 +61,34 @@ class Condition:
 
     def generate_new(self):
         for neighbour in self.neighbours():
+            new_scheme = self.scheme.copy()
             if neighbour[1] == 1:
-                self.scheme[(self.pos_y + neighbour[0][1]) * WIDTH + self.pos_x + neighbour[0][0]] = '⬜'
-                self.scheme[(self.pos_y + 2 * neighbour[0][1]) * WIDTH + self.pos_x + 2 * neighbour[0][0]] = '▧'
-            conditions.append(Condition(self.scheme,
+                new_scheme[(self.pos_y + neighbour[0][1]) * WIDTH + self.pos_x + neighbour[0][0]] = '⬜'
+                new_scheme[(self.pos_y + 2 * neighbour[0][1]) * WIDTH + self.pos_x + 2 * neighbour[0][0]] = '▧'
+            elif neighbour[1] == 2:
+                new_scheme[(self.pos_y + neighbour[0][1]) * WIDTH + self.pos_x + neighbour[0][0]] = '◑'
+                self.gen_count -= 1
+            conditions.append(Condition(new_scheme,
                                         self.pos_x + (neighbour[0][0] if not neighbour[1] else 0),
                                         self.pos_y + (neighbour[0][1] if not neighbour[1] else 0),
                                         self.turn + 1,
                                         self.route + [neighbour],
+                                        self.gen_count,
                                         False))
 
 
-conditions.append(Condition(list(MAP), POS_X, POS_Y, 0, [], False))
+conditions.append(Condition(list(MAP), POS_X, POS_Y, 0, [], GEN_COUNT, False))
 fastest = TURN_LIMIT + 1
-best_condition = None
-solutions = 0
+solutions = []
 for condition in conditions:
+    if DEBUG:
+        print(condition.turn, condition.route)
     if condition.done and condition.turn < fastest:
-        best_condition = condition
         fastest = condition.turn
-        solutions = 1
+        solutions = [condition]
     elif condition.done and condition.turn == fastest:
-        solutions += 1
+        solutions.append(condition)
 print("Checked", len(conditions), "possible condition..." if len(conditions) == 1 else "possible conditions....")
-print("Found", solutions, "solution." if solutions == 1 else "solutions.")
-print("Solution example:", best_condition.turn, best_condition.route)
+print("Found", len(solutions), "solution." if len(solutions) == 1 else "solutions.")
+for solution in solutions:
+    print(solution.turn, solution.route)
